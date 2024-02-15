a4506297c186feece2573cf807ddffd443e1b700
Simon Prickett <simon@crudworks.org>
2024-02-15 23:29:31 +0000

    Added some more request headers.

--- index.js
+++ index.js
@@ -62,7 +62,10 @@ async function handleRequest(request) {
   const stopUrl = `https://www.nctx.co.uk/stops/${stopId}`
   const stopPage = await fetch(stopUrl, {
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+      'Accept': '*/*',
+      'Accept-Language': 'en-US',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+      'Referer': stopUrl
     }
   })
 
